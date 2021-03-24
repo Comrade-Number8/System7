fcdab5d84552ac54c6fb05d0b284a19f730d5226
Rogue <rogue.system7@protonmail.com>
2021-03-24 04:50:48 +0100

    z

---
+++ no/Rogue.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="no" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>System_7</title>
+    <link rel="shortcut icon" href="/System_7/Images/ico-static/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/System_7/Images/ico-animated/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/System_7/css/master.css">
+    <link rel="stylesheet" href="/System_7/css/arkive.css">
+    <script src="/System_7/JavaScript/ImpHTML.js" charset="utf-8"></script>
+</head>
+<body>
+    <div w3-include-html="/System_7/no/ImpHTML/header.html"></div>
+    <div w3-include-html="/System_7/no/ImpHTML/nav-bar.html"></div>
+    <div class="MC-row">
+        <div class="MainContent MC-Left">
+            <p></p>
+        </div>
+        <div class="MainContent MC-Main">
+            <h1>Rogue</h1>
+            <?php
+            echo "Rogue.System7@protonmail.com";
+            ?>
+        </div>
+        <div class="MainContent MC-Right">
+            <p></p>
+        </div>
+    </div>
+    <script>
+      includeHTML();
+    </script>
+</body>
+</html>
